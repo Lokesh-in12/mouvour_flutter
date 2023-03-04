5af9fe822f1d7f05870eabe84c1258801d70f2c4
Lokesh-in12 <fixpoint.lokesh@gmail.com>
2023-03-04 18:16:07 +0530

    workig home page

--- lib/logic/cubits/movie_cubit.dart
+++ lib/logic/cubits/movie_cubit.dart
@@ -6,40 +6,28 @@ import 'package:mouvour_flutter/logic/cubits/movie_state.dart';
 class MovieCubit extends Cubit<MovieState> {
   MovieCubit() : super(MovieLoadingState()) {
     fetchNowPlaying();
-    
   }
 
   MovieRepository movieRepository = MovieRepository();
 
   void fetchNowPlaying() async {
     try {
-      print("in fetchNowPlaying()");
+      //now_playing
       List<MovieModel> nowMovies = await movieRepository.fetchNowPlaying();
-      print("after getting data and bfore emiiting $nowMovies");
-      print("=====>>>>> $nowMovies");
       //popular
-      print("in fetchPopular()");
       List<MovieModel> popular_movies = await movieRepository.fetchPopular();
-      print("after getting data and bfore emiiting $popular_movies");
-      emit(MovieLoadedState(popular_movies: popular_movies));
-      print("=====>>>>> $popular_movies");
 
       //toprated
-      print("in fetchTopRated()");
       List<MovieModel> top_rated = await movieRepository.fetchTopRated();
-      print("after getting data and bfore emiiting $top_rated");
-      emit(MovieLoadedState(top_rated_movies: top_rated));
-      print("toprated $top_rated");
 
       //trending
-      print("in fetchTrending()");
       List<MovieModel> trending_movies = await movieRepository.fetchTrending();
-      print("after getting data and bfore emiiting $trending_movies");
-      emit(MovieLoadedState(trending_movies: trending_movies));
-      print("trending $trending_movies");
 
-
-      emit(MovieLoadedState(now_playing_movies: nowMovies, popular_movies: popular_movies , top_rated_movies: top_rated ,trending_movies: trending_movies));
+      emit(MovieLoadedState(
+          now_playing_movies: nowMovies,
+          popular_movies: popular_movies,
+          top_rated_movies: top_rated,
+          trending_movies: trending_movies));
     } catch (e) {
       print("error is $e");
       emit(MovieErrorState(e.toString()));
